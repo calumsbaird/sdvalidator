ede12390b25bd321467ee3fca359762783281b8d
calumsbaird <calum.s.baird@gmail.com>
2019-02-05 15:04:06 +1100

    nothing special

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Calum Baird'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.1.12'
+release = '2.1.14'
 
 
 # -- General configuration ---------------------------------------------------
